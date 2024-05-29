822920bf29c14340ff37a0399fc6c4471a55fac6
Weazzon <camoletik1@gmail.com>
2024-05-30 00:40:09 +0300

    Update test_selenium_petfriends.py

--- test_selenium_petfriends.py
+++ test_selenium_petfriends.py
@@ -3,8 +3,6 @@ from selenium.webdriver.support.ui import WebDriverWait as WDW
 from selenium.webdriver.support import expected_conditions as EC
 
 
-
-
 def test_show_my_pets(selenium_driver):
     '''Этот тест проверяет, что на сайте присутствуют все питомцы пользователя'''
     driver = selenium_driver
@@ -12,8 +10,6 @@ def test_show_my_pets(selenium_driver):
     #Явные ожидания
     WDW(driver, 2).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="navbarNav"]/ul/li[1]/a')))
     driver.find_element(By.XPATH,'//*[@id="navbarNav"]/ul/li[1]/a').click()
-
-
 
     #Находим количество питомцев, отображенную на сайте
     WDW(driver, 2).until(EC.visibility_of_element_located((By.XPATH, '//div[@class=".col-sm-4 left"]')))
@@ -36,15 +32,11 @@ def test_photo_pets(selenium_driver):
 
     pets_count = driver.find_elements(By.XPATH, '//table[@class="table table-hover"]/tbody/tr')
     #Находим питомцев у которых есть фото
-
     # Неявные ожидания
     driver.implicitly_wait(2)
     image_count = driver.find_elements(By.XPATH, '//img[starts-with(@src, "data:image/")]')
 
-
     assert len(image_count) > ((len(pets_count) % 2) == 0),f'Количество питомцев с фото {len(image_count)} составляет меньше половины всех моих питомцев {len(pets_count)}'
-
-
 
 
 def test_allpet_have_name_breed_age(selenium_driver):
